02f6b26c8a7e0291845a6705adba330910dea269
alsrl8259 <alsrl8259@gmail.com>
2021-01-07 00:00:33 +0900

    NWEETING

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,12 @@ import {authService} from "fbase";
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user)=>{
       if(user){
         setIsLoggedIn(true);
+        setUserObj(user);
       } else{
         setIsLoggedIn(false);
       }
@@ -18,7 +20,11 @@ function App() {
 
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Intializing..." }
+      {init ? (
+        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+      ) : (
+        "Intializing..."
+      )}
       <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
     </>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,53 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
 
- const Home = () => <span>Home</span>;
+ const Home = ({ userObj }) => {
+   const [nweet, setNweet] = useState("");
+   const [nweets, setNweets] = useState([]);
+   useEffect(() => {
+     dbService.collection("nweets").onSnapshot(snapshot=>{
+         const nweetArray = snapshot.docs.map((doc) => ({
+           id: doc,
+           ...doc.data(),
+         }));
+         setNweets(nweetArray);
+     })
+   }, []);
+   const onSubmit = async (event) => {
+     event.preventDefault();
+     await dbService.collection("nweets").add({
+       text: nweet,
+       createdAt: Date.now(),
+       creatorId: userObj.uid,
+     });
+     setNweet("");
+   };
+   const onChange = (event) => {
+     const {
+       target: { value },
+     } = event;
+     setNweet(value);
+   };
+   return (
+     <div>
+       <form onSubmit={onSubmit}>
+         <input
+           value={nweet}
+           onChange={onChange}
+           type="text"
+           placeholder="What's on your mind?"
+           maxLength={120}
+         />
+         <input type="submit" value="Nweet" />
+       </form>
+       <div>
+         {nweets.map((nweet) => (
+           <div key={nweet.id}>
+             <h4>{nweet.text}</h4>
+           </div>
+         ))}
+       </div>
+     </div>
+   );
+ };
  export default Home;
